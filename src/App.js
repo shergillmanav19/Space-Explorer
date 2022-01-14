90b662deb40355fbe7b9f4afefee6ba1b90a2d67
Manav Shergill <shergillmanav@uvic.ca>
2022-01-13 20:48:13 -0800

    First commit; added boiler plates

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import useLocalStorage from "./hooks/useLocalStorage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import DashboardPage from "./pages/DashboardPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/dashboard" element={<DashboardPage />} />
+        <Route path="/*" element={<LandingPage />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/api/Api.js
@@ -0,0 +1,25 @@
+const getImagesFromAPI = async ({
+  start_date = "",
+  end_date = "",
+  count = "",
+  thumbs = "",
+}) => {
+  let data = { status: "loading", data: {} };
+  try {
+    const response = await fetch(
+      `https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_NASA_API_KEY}&count=${count}&start_date=${start_date}&end_date=${end_date}&thumbs=${thumbs}`
+    );
+
+    const response_data = await response.json();
+    data["status"] = "Success";
+    data["data"] = response_data;
+    console.log(data);
+    return data;
+  } catch (e) {
+    data["status"] = "Error";
+    console.warn(e);
+    return data;
+  }
+};
+
+export default getImagesFromAPI;
