3e719a1da04d41ca947b3ec122dab0b89993a940
Manav Shergill <shergillmanav@uvic.ca>
2022-01-14 19:58:41 -0800

    Added error component

---
+++ src/components/Error.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+import {
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  AlertTitle,
+  CloseButton,
+} from "@chakra-ui/react";
+
+function Error({ myError, setError }) {
+  return (
+    <>
+      {myError !== "" ? (
+        <Alert status="error">
+          <AlertIcon />
+          <AlertTitle mr={2}>An error occured!</AlertTitle>
+          <AlertDescription>{myError}</AlertDescription>
+          <CloseButton
+            onClick={() => setError("")}
+            position="absolute"
+            right="8px"
+            top="8px"
+          />
+        </Alert>
+      ) : (
+        ""
+      )}
+    </>
+  );
+}
+
+export default Error;
